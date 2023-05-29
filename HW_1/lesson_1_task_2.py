ef0dd948f008fc7f0cb5bb224be7e79c634d0828
DemonOfFreeride <demon.of.freeride@gmail.com>
2023-05-29 20:14:00 +0300

    Добавил еомментарии

--- HW_1/lesson_1_task_2.py
+++ HW_1/lesson_1_task_2.py
@@ -1,4 +1,4 @@
 my_age = 10
 print(my_age)
-my_age = (my_age) + 3
+my_age = my_age + 3  # при перезаписывании переменной не обязательно ставить ее в скобки
 print(my_age)
--- HW_1/lesson_1_task_7.py
+++ HW_1/lesson_1_task_7.py
@@ -2,7 +2,7 @@ def tel_Sky(num):
     return str(num)
 
 result = tel_Sky(8) + tel_Sky(8) + tel_Sky(0) + tel_Sky(0) + tel_Sky(5) + tel_Sky(5) + tel_Sky(5) + tel_Sky(3) + tel_Sky(5) + tel_Sky(3) + tel_Sky(5)
-print(result)
+print(result)   #  - на практике вызывать множество функций в одну строку - плохой вариант (из-за плохой читаемости)
 
 # -  Другой способ не в строчку
 
