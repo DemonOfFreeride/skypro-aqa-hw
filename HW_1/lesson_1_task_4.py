a6a08ce69889495d8baf7670453cb188995fc1a7
DemonOfFreeride <demon.of.freeride@gmail.com>
2023-06-03 14:58:47 +0300

    + f-строка

--- HW_1/lesson_1_task_4.py
+++ HW_1/lesson_1_task_4.py
@@ -1,3 +1,15 @@
 first_name = input("First name: ")
 last_name = input("Last name: ")
 print("Your name is: " + first_name + " " + last_name)
+
+# тоже самое только с применением f-строк в Python > 3.6 версиях
+
+first_name = input("First name: ")
+last_name = input("Last name: ")
+print(f"Your name is: {first_name} + {last_name}")
+
+# Можно вывести переменную и ее значение с помощью f-строки 
+
+first_name = "Freddy"
+last_name = "Mercury"
+print(f"{first_name=} {last_name=}")
