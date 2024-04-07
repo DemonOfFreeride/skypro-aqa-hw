24f0f1bcb4f7c9395b17b669c481ecd9448b698b
Valeriy.K <kazakov.vv@svitok31.ru>
2024-04-07 17:38:18 +0300

    dell_wrong_hw8

--- HW/HW_8/test_x_clients_employee.py
+++
@@ -1,7 +0,0 @@
-from EmployeeApi import EmployeeApi
-
-api = EmployeeApi("https://x-clients-be.onrender.com")
-
-def test_get_employees_companies():
-    body = api.get_company_employee(3219)
-    return body
